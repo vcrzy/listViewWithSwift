8f4a560c0cf97affe72b9d9216bd1dab7beec4e2
viniciusmontanheiro <mrvini@live.com>
2015-07-02 10:22:29 -0300

    List View

---
+++ listViewWithSwift/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  listViewWithSwift
+//
+//  Created by Vinícius Montanheiro on 02/07/15.
+//  Copyright (c) 2015 Vinícius Montanheiro. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var descricao: UILabel!
+
+}
---
+++ listViewWithSwift/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  listViewWithSwift
+//
+//  Created by Vinícius Montanheiro on 02/07/15.
+//  Copyright (c) 2015 Vinícius Montanheiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+
+    var pessoas = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        for i in 0...1000 {
+            self.pessoas.append("Fulano \(i)")
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var celula = tableView.dequeueReusableCellWithIdentifier("Celula", forIndexPath: indexPath) as! CustomTableViewCell
+        
+        celula.descricao.text = pessoas[indexPath.row]
+        
+        return celula
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.pessoas.count
+    }
+
+}
+
